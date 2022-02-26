6324207fd05b68502f1bfefbc2befcc3d48d7e17
hamza AQQA <hamzaaqqa7@gmail.com>
2022-02-26 19:38:51 +0100

    snapchot setting

---
+++ 04-Snapchots/main.tf
@@ -0,0 +1,8 @@
+##################################################################################
+# PROVIDERS
+##################################################################################
+provider "aws" {
+  region = "us-east-1"
+    secret_key = "AKIAYN3QUXTB2VTC537U"
+  access_key = "OuwmuGvWqvDBwI0trBPEnlzjAJ+NDGbmPX83Oan0"
+}
